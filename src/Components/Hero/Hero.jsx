9714ac6374a561858ea9eddcc2787e06ba31eec9
Janhavi-31 <janvinarkhede04@gmail.com>
2025-09-27 01:22:59 +0530

    Adding Offer page

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Hero.css'
 import hand_icon from '../Assets/Frontend_Assets/hand_icon.png'
 import arrow_icon from '../Assets/Frontend_Assets/arrow.png'
-import hero_image from "../Assets/Frontend_Assets/hero_image.png"
+import hero_image from '../Assets/Frontend_Assets/hero_image.png'
 
 const Hero = () => {
   return (
