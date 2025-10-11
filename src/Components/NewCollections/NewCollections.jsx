392f8a26f5cacf96612b96e2b65b0abf1829e528
Janhavi-31 <janvinarkhede04@gmail.com>
2025-10-11 23:18:33 +0530

    Working on Description Box

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './NewCollections.css'
 import new_collection from '../Assets/Frontend_Assets/new_collections'
-
+import Item from '../Item/Item'
 const NewCollections = () => {
   return (
     <div className='new-collections'>
--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/Frontend_Assets/all_product";
 export const ShopContext = createContext(null);
 const ShopContextProvider = (props) => {
